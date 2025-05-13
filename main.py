6e5ede2f5f737b4aedd5e6a35708130d7af048fb
JKukielski <jankukielski1@gmail.com>
2025-05-13 18:51:07 +0200

    list learning

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-user_text = input("Enter todo: ")
-print(user_text)
+prompt = "Enter todo: "
+todo1 = input(prompt)
+todo2 = input(prompt)
+todo3 = input(prompt)
+
+todos = [todo1, todo2, todo3]
+
+print(todos)

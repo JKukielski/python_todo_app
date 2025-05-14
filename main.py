5d9222a9c42d7cc5f9602af15f1425de38d613bd
JKukielski <jankukielski1@gmail.com>
2025-05-14 20:10:50 +0200

    while loop practice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 prompt = "Enter todo: "
-todo1 = input(prompt)
-todo2 = input(prompt)
-todo3 = input(prompt)
-
-todos = [todo1, todo2, todo3]
-
-print(todos)
+todos = []
+while True:
+    todo = input(prompt)
+    todos.append(todo)
+    print(todos)

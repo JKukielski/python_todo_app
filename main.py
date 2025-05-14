784419fcb366c516b6e11241371c4f99cc9e9f60
JKukielski <jankukielski1@gmail.com>
2025-05-14 20:25:16 +0200

    while loop practice pt.2

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ while True:
     todo = input(prompt)
     todos.append(todo)
     print(todos)
+
+#password checker
+password = input("Enter password: ")
+while password != "pass123":
+    password = input("Enter password: ")
+
+print("Password correct!")
+
+#series of numbers
+x = 1
+while x <= 6:
+    print(x)
+    x = x + 1

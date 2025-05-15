6779db591ebb6bc754b186b562664712645f5843
JKukielski <jankukielski1@gmail.com>
2025-05-15 18:32:38 +0200

    expanded todo app - added edit option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 todos = []
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
@@ -9,5 +9,12 @@ while True:
         case 'show':
             for item in todos:
                 print(item)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'exit':
             break
+        case _:
+            print("Unknown command, please try again.")

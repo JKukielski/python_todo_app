13f748d9947c58e6339bdd9ec67b53f9b81ef83f
JKukielski <jankukielski1@gmail.com>
2025-05-20 14:54:05 +0200

    Converting match case to if/elif/else

--- main.py
+++ main.py
@@ -1,58 +1,75 @@
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
-    match user_action:
-        case 'add':
-            todo = input("Enter a todo: ") + "\n"
-            # file = open('todos.txt', 'r')
-            # todos = file.readlines()
-            # file.close()
 
-            with open('todos.txt','r') as file:
-                todos = file.readlines()
+    if 'add' in user_action:
+        todo = user_action[4:]
+        # file = open('todos.txt', 'r')
+        # todos = file.readlines()
+        # file.close()
 
-            todos.append(todo)
+        with open('todos.txt','r') as file:
+            todos = file.readlines()
 
-            # file = open('todos.txt', 'w')
-            # file.writelines(todos)
-            # file.close()
+        todos.append(todo)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-        case 'show':
-            # file = open('todos.txt', 'r')
-            # todos = file.readlines()
-            # file.close()
+        # file = open('todos.txt', 'w')
+        # file.writelines(todos)
+        # file.close()
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+    elif 'show' in user_action:
+        # file = open('todos.txt', 'r')
+        # todos = file.readlines()
+        # file.close()
 
-            # new_todos = []
-            # for item in todos:
-            #     new_item = item.strip('\n')
-            #     new_todos.append(new_item)
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # new_todos = [item.strip('\n') for item in todos]
+        # new_todos = []
+        # for item in todos:
+        #     new_item = item.strip('\n')
+        #     new_todos.append(new_item)
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index + 1}-{item}"
-                print(row)
-        case 'edit':
-            # file = open('todos.txt', 'r')
-            # todos = file.readlines()
-            # file.close()
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        # new_todos = [item.strip('\n') for item in todos]
 
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
-        case 'exit':
-            break
-        case _:
-            print("Unknown command, please try again.")
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    elif 'edit' in user_action:
+        # file = open('todos.txt', 'r')
+        # todos = file.readlines()
+        # file.close()
+
+
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        new_todo = input("Enter new todo: ")
+        todos[number] = new_todo + "\n"
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+    elif "complete" in user_action:
+        number = int(input("Number of the todo to complete: "))
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        todo_to_remove = todos[number - 1].strip('\n')
+        todos.pop(number - 1)
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+        message = f"Todo {todo_to_remove} was removed from the list"
+        print(message)
+    elif 'exit' in user_action:
+        break
+    else:
+        print('Command is not valid')
